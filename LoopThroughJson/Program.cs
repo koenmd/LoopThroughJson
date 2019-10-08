2978899431dd43e0a6243f62e285e0b6b1b1d737
koent <koent@DESKTOP-G9HKP2T>
2019-10-08 23:32:10 +0200

    Add project files.

---
+++ LoopThroughJson/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+using System.ComponentModel;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (System.IO.File.Exists(@".\App_Data\en.json"))
+            {
+                using (StreamReader file = File.OpenText(@".\App_Data\en.json"))
+                {
+                    var jObject = JsonConvert.DeserializeObject<JObject>(file.ReadToEnd());
+                    if (jObject != null && jObject.Children().Any())
+                    {
+                        foreach (var child in jObject.Children().ToList())
+                        {
+                            LoopThroughJTokens(child);
+                        }
+                    }
+
+
+                }
+            }
+
+
+            Console.ReadKey();
+        }
+
+        static void LoopThroughJTokens(JToken jToken)
+        {
+            if(jToken != null && jToken.Children().Any())
+            {
+                foreach(var child in jToken.Children().ToList())
+                {
+                    if (child.GetType() == typeof(JValue))
+                    {
+                        Console.WriteLine(((JProperty)child.Parent).Name + " : " + child.ToString() + " [" + child.Path +  "]") ;
+                    }
+                    else
+                        LoopThroughJTokens(child);
+                }
+            }
+        }
+
+    }
+}
